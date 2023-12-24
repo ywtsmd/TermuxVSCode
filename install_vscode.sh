ca505df8aaaca8cdc3616273dc2bf17be0951336
ywtsmd <137508851+ywtsmd@users.noreply.github.com>
2023-12-25 01:16:15 +0500

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -13,10 +13,3 @@ pkg install proot-distro -y
 
 proot-distro install ubuntu # Install Ubuntu
 proot-distro login ubuntu # Login Ubuntu
-sudo apt-get update # Refresh repo
-sudo apt-get install wget -y # Install wget
-
-wget https://github.com/coder/code-server/releases/download/v4.20.0/code-server-4.20.0-linux-arm64.tar.gz
-tar -xzvf code-server-4.20.0-linux-arm64.tar.gz # Extract archive
-
-echo -e "Start start_vscode.sh"
